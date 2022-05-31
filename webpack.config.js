45e62b92de29c6947534d6cd85a55aed2ba758b8
devloliconic <kirillevsmirnov@gmail.com>
2022-05-31 17:24:46 +0700

    add files

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output:{
+        filename: `${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+    },
+    devServer:{
+        historyApiFallback: true,
+        //contentBase: path.resolve(__dirname, 'app'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify:{
+                collapseWhitespace: isProd
+            }
+        }),
+         new CleanWebpackPlugin(),
+         new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+         }),
+        //  new CopyWebpackPlugin({
+        //      patterns: [
+        //          {from: path.resolve(__dirname, 'src/assets') , to: path.resolve(__dirname, 'app')}
+        //      ]
+        //  }),
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+                type: 'asset/inline'
+            }
+        ]
+    },
+};
